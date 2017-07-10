4b7f287e340298dbcf827a3e456cf41579a1ca01
juliazxu <juliazxu@gmail.com>
2017-07-10 09:42:36 -0700

    Changed gameover and gamewon

--- src/App.js
+++ src/App.js
@@ -6,23 +6,21 @@ let word = "apple";
 let wordArr = word.toUpperCase().split('');
 
 const TriesLeft = props => {
-  if ( props.triesRemaining !== 0) {
-    if (props.gameWon) {
-    return (
-      <h3>You guessed it! Good work.</h3>
-   )
-  } else {
-    return (
-      <h3>Tries Left: {props.triesRemaining}</h3>
-    )
-  }
-  } else {
+  if (props.triesRemaining === 0) {
     return (
       <div className="flexcontainer">
         <h3>Game over!</h3>
         <button onClick={props.showWord}>Show Word</button>
       </div>
     )
+  } else {
+    if (!props.guessedWord.includes('_')) {
+      console.log('Won?')
+      return (<h3>You guessed it! Good work.</h3>)
+    } else {
+      console.log(props.guessedWord)
+      return (<h3>Tries Left: {props.triesRemaining}</h3>)
+    }
   }
 }
 
@@ -70,8 +68,6 @@ class App extends Component {
       lettersGuessed: [],
       guessedWord: ['_', '_', '_', '_', '_'],
       triesRemaining: 8,
-      gameOver: false,
-      gameWon: false,
     };
 
     this.enterLetter = this.enterLetter.bind(this);
@@ -94,9 +90,9 @@ class App extends Component {
         document.getElementById('inputLetter').value = '';
       }
       else {
-        prevState.lettersGuessed.push(l);
-        prevState.triesRemaining = prevState.triesRemaining - 1;
-        document.getElementById('inputLetter').value = '';
+          prevState.lettersGuessed.push(l);
+          prevState.triesRemaining = prevState.triesRemaining - 1;
+          document.getElementById('inputLetter').value = '';
       }
     });
   }
@@ -105,26 +101,13 @@ class App extends Component {
     this.setState ({guessedWord: wordArr});
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    if ( !this.state.guessedWord.includes('_')) {
-      this.setState({ gameWon: true })
-      document.getElementById('inputLetter').disabled = true;
-      document.getElementById('submit').disabled = true;   
-    }
-    if (this.state.triesRemaining === 0) {
-      this.setState({ gameOver: true })
-      document.getElementById('inputLetter').disabled = true;
-      document.getElementById('submit').disabled = true;
-    }
-  }
-
   render() {
     return (
       <div className="flexcontainer">
         <h1>Hangman</h1>
-        <TriesLeft gameWon = {this.state.gameWon} showWord = {this.showWord} triesRemaining = {this.state.triesRemaining} lettersGuessed = {this.state.lettersGuessed} />
+        <TriesLeft guessedWord = {this.state.guessedWord} showWord = {this.showWord} triesRemaining = {this.state.triesRemaining} lettersGuessed = {this.state.lettersGuessed} />
         <GuessWord guessedWord = {this.state.guessedWord} />
-        <InputBox checkWon = {this.checkWon} enterLetter = {this.enterLetter} checkWon = {this.checkWon} />
+        <InputBox enterLetter = {this.enterLetter} checkWon = {this.checkWon} />
         <LettersTried lettersGuessed = {this.state.lettersGuessed} />
       </div>
     );
